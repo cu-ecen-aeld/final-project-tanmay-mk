2a0ced9f57d3427c87b7d3da634324f1b994a88e
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2022-04-05 15:46:14 -0600

    socket server application imported

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,4 +1,3 @@
-
 ##############################################################
 #
 # AESD-ASSIGNMENTS
@@ -6,19 +5,21 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+AESD_ASSIGNMENTS_VERSION = d38b58a1bbfe924d3cebec0b02457072f9821b1e
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/final-project-Amey2904dash.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
-
+AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/sample_socket/socket_server all
 endef
 
-#TODO: Add required executables or scripts below
+# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/sample_socket/socket_server/socket_server $(TARGET_DIR)/usr/bin
 endef
-
 
 $(eval $(generic-package))
