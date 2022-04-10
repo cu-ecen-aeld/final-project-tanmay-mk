d87dca1fed1fbc644df8bbbc3dfb88d7f69b21f9
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2022-04-10 17:33:33 -0600

    gpio package added for LCD

--- base_external/package/gpio_package/gpio_package.mk
+++ base_external/package/gpio_package/gpio_package.mk
@@ -1,25 +1,25 @@
 ##############################################################
 #
-# AESD-ASSIGNMENTS
+# GPIO-PACKAGE
 #
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 67f5c65ecb245e3d723776a0fe163e85b31bc605
+GPIO_PACKAGE_VERSION = 3d54e0f14446d055ee69f0b9c171d1fc9bbfa9f7
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/final-project-Amey2904dash.git
-AESD_ASSIGNMENTS_SITE_METHOD = git
-AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+GPIO_PACKAGE_SITE = git@github.com:cu-ecen-aeld/final-project-Amey2904dash.git
+GPIO_PACKAGE_SITE_METHOD = git
+GPIO_PACKAGE_GIT_SUBMODULES = YES
 
-define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/sample_socket/socket_client all
+define GPIO_PACKAGE_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/gpio all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/sample_socket/socket_client/socket_client $(TARGET_DIR)/usr/bin
+define GPIO_PACKAGE_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/gpio/lcd $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
